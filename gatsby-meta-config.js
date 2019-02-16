241f8dc19f51e75a17bff3fe488047963e8c7b25
hados <hados@sbs.co.kr>
2019-02-17 00:14:57 +0900

    설정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: ``, // Your blog site url
+  title: `Hados Blog`,
+  description: `Hados Blog about development, media, my views`, 
+  author: `hados`,
+  introduction: ``,
+  siteUrl: `https://hados99.github.io`, // Your blog site url
   social: {
-    twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    twitter: `hados99`, // Your Twitter account
+    github: `hados99`, // Your GitHub account
     medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
+    facebook: `Doyoung Ha`, // Your Facebook account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: 'hados-blog-1', // Your disqus-short-name. check disqus.com.
+    utterances: '', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
@@ -25,5 +25,5 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '', // Add your google analytics tranking ID
+  ga: 'UA-125631114-1', // Add your google analytics tranking ID
 }
